5ed6ab9ad5c79d41c7ae8c7f8a392cd1b6ee96f3
Simon Gawlik <sgawlik@gmail.com>
2016-01-25 12:33:09 -0500

    added ex43

--- ex42.py
+++ ex42.py
@@ -75,3 +75,19 @@ crouse = Salmon()
 ## harry is-a Halibut
 harry = Halibut()
 
+
+"""
+1. Research why Python added this strange object class, and what that means.
+
+2. Is it possible to use a class like it's an object?
+
+3. Fill out the animals, fish, and people in this exercise with functions that make them do things. See what happens when functions are in a "base class" like Animal versus in say Dog.
+
+4. Find other people's code and work out all the is-a and has-a relationships.
+
+5. Make some new relationships that are lists and dictionaries so you can also have "has-many" relationships.
+
+6. Do you think there's such thing as an "is-many" relationship? Read about "multiple inheritance," then avoid it if you can.
+
+"""
+
